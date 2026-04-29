d1971af5e63f2fde4a7aa79dd3f48a0b11795bd7
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2026-04-29 16:30:28 -0400

    update pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.github.io/salsa-roja',
+  site: 'https://avivajpeyi.github.io',
+  base: '/salsa-roja',
   output: 'static',
 });
